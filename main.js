b23cef521f485d8bde0ae047feda603aa0ef5ea9
Died <marvink9378@gmail.com>
2023-06-30 18:14:17 -0600

    implementacion de funciones a menu mobile con JS

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
 const menuEmail = document.querySelector('.navbar-email');
 const DesktopMenu = document.querySelector('.desktop-menu');
+const mobileMenu = document.querySelector('.mobile-menu')
+const burguerMenu = document.querySelector('.burguermenu') 
 
 menuEmail.addEventListener('click',toggleDesktopMenu);
+burguerMenu.addEventListener('click',toogleMobileMenu);
 
 function toggleDesktopMenu(){
     DesktopMenu.classList.toggle('inactive');
+}
+function toogleMobileMenu(){
+    mobileMenu.classList.toggle('inactivemobile');
 }
